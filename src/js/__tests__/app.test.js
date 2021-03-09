dda41ddabebe658c5b53d2b7e78334aee3627624
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-09 21:51:31 +0300

    Добавил проверку toBe

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -12,6 +12,21 @@ test('sort index health level', () => {
 		{ name: 'мечник', health: 10 },
 	];
 	const received = sortHealth(users);
+	expect(received).toBe(expected);
+});
+
+test('sort index health level', () => {
+	const users = [
+		{ name: 'мечник', health: 10 },
+		{ name: 'маг', health: 100 },
+		{ name: 'лучник', health: 80 },
+	];
+	const expected = [
+		{ name: 'маг', health: 100 },
+		{ name: 'лучник', health: 80 },
+		{ name: 'мечник', health: 10 },
+	];
+	const received = sortHealth(users);
 	expect(received).toEqual(expected);
 });
 
