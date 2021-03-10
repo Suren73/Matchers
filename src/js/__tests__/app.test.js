b07b2ac0636b6bfd8d851c8cb23ad0ed6b07f258
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-10 17:51:01 +0300

    Внесены изменения --fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,61 +1,61 @@
 import sortHealth from '../app';
 
 test('sort index health level', () => {
-	const users = [
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-	];
-	const expected = [
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-		{ name: 'мечник', health: 10 },
-	];
-	const received = sortHealth(users);
-	expect(received).toBe(expected);
+  const users = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const received = sortHealth(users);
+  expect(received).toBe(expected);
 });
 
 test('sort index health level', () => {
-	const users = [
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-	];
-	const expected = [
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-		{ name: 'мечник', health: 10 },
-	];
-	const received = sortHealth(users);
-	expect(received).toEqual(expected);
+  const users = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const received = sortHealth(users);
+  expect(received).toEqual(expected);
 });
 
 test('sort index health level', () => {
-	const users = [
-		{ name: 'мечник', health: 100 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 100 },
-	];
-	const expected = [
-		{ name: 'мечник', health: 100 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 100 },
-	];
-	const received = sortHealth(users);
-	expect(received).toEqual(expected);
+  const users = [
+    { name: 'мечник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 100 },
+  ];
+  const expected = [
+    { name: 'мечник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 100 },
+  ];
+  const received = sortHealth(users);
+  expect(received).toEqual(expected);
 });
 
 test('sort index health level', () => {
-	const users = [
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 10 },
-		{ name: 'лучник', health: 100 },
-	];
-	const expected = [
-		{ name: 'лучник', health: 100 },
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 10 },
-	];
-	const received = sortHealth(users);
-	expect(received).toEqual(expected);
+  const users = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 10 },
+    { name: 'лучник', health: 100 },
+  ];
+  const expected = [
+    { name: 'лучник', health: 100 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 10 },
+  ];
+  const received = sortHealth(users);
+  expect(received).toEqual(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 export default function sortHealth(data) {
-	const sortUsers = [];
-	Object.keys(data)
-		.sort((a, b) => data[b].health - data[a].health)
-		.forEach((index) => sortUsers.push(data[index]));
-	return sortUsers;
+  const sortUsers = [];
+  Object.keys(data)
+    .sort((a, b) => data[b].health - data[a].health)
+    .forEach((index) => sortUsers.push(data[index]));
+  return sortUsers;
 }
